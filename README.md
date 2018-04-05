e369f1fe53e426fe8d3239272aa73c8d8739ee93
mjkang <mjkang@motrex.co.kr>
2018-04-05 17:23:17 +0900

    modify type

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # DCS (Daegu Chicken System)
 
 ```
-
+1. npm install
+2. cd backend
+3. npm install
+4. cd ..
+5. cd frontend
+6. npm install
+7. cd ..
+8. npm start
 ```
--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,11 @@
 const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
+const winston = require('winston');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-const winston = require('winston');
+
 const index = require('./routes/index');
-const logger = new (winston.Logger)({
-    transports: [
-        new (winston.transports.Console)({
-            timestamp: tsFormat
-        })
-    ]
-});
 
 const app = express();
 
@@ -27,7 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use(require('connect-history-api-fallback')())
+app.use(require('connect-history-api-fallback')());
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
